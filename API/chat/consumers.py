99e050e8c20590959c6a98c9befecdf486da2eb2
unknown <dario.patt2@gmail.com>
2022-02-13 21:27:43 +0100

    add authtoken authorization to chat websocket

--- API/chat/consumers.py
+++ API/chat/consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 from .models import Chat
 
@@ -7,10 +9,16 @@ from .models import Chat
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
-        self.user = self.scope["user"] # TODO: use token autentication
+
+        token = dict(self.scope["headers"])[b"authorization"].decode().split()[1]
         
-        print(self.user)
-        
+        try:
+            token = await sync_to_async(Token.objects.get, thread_sensitive=True)(key=token)
+            self.user = await sync_to_async(get_user_model().objects.get, thread_sensitive=True)(auth_token=token)
+        except Token.DoesNotExist:
+            await self.close()
+            return
+            
         try:
             chat = await sync_to_async(Chat.objects.get, thread_sensitive=True)(pk=self.chat_id)
             self.room_name = chat.pk
@@ -18,16 +26,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except Chat.DoesNotExist:
             await self.close()
             return
-        
+            
         if not self.user.is_authenticated:
             await self.close()
             return
-        
+            
         if not await sync_to_async(chat.is_member, thread_sensitive=True)(self.user):
             await self.close()
             return
-
-        self.accept()
+            
+        await self.accept()
     
     async def disconnect(self, close_code):
         pass
